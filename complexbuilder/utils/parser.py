be0976287029aaa98f7bdcfb874b1949be2fb8e5
YoshitakaMo <virgospica93@gmail.com>
2024-10-21 14:28:30 +0900

    1021

--- complexbuilder/utils/parser.py
+++ complexbuilder/utils/parser.py
@@ -1,6 +1,7 @@
 # %%
 import json
 import re
+from pathlib import Path
 
 from Bio import SeqIO
 from Bio.Seq import Seq
@@ -8,11 +9,27 @@ from Bio.SeqFeature import FeatureLocation, SeqFeature
 from Bio.SeqRecord import SeqRecord
 
 
-def parse_mibig_json(mibig_json: str) -> dict:
-    """Parse a MIBiG JSON file and return a dictionary with the parsed data."""
-    with open(mibig_json, "r") as f:
-        mibig_data = json.load(f)
-    return mibig_data
+def combination_of_sequence(genbank_file: str | Path, mibig_json: str | Path):
+    """Combine protein records from GenBank to return protein pairs
+    for multimer complex building.
+
+    There are seven "biosyn_class" values (Secondary Metabolite record type)
+    in MIBiG JSON files:
+      1. Polyketide
+      2. NRP
+      3. RiPP
+      4. Terpene
+      5. Saccharide
+      6. Alkaloid
+      7. Other
+
+    inputs:
+    - genbank_file (str | Path): Path to the GenBank file.
+    - mibig_json (str | Path): Path to the MIBiG JSON file.
+    returns:
+    - protein_pairs: List of protein pairs.
+    """
+    pass
 
 
 # %%
@@ -41,4 +58,22 @@ for record in SeqIO.parse(genbank_file, "genbank"):
             gene = feature.qualifiers.get("gene", ["N/A"])[0]
             product = feature.qualifiers.get("product", ["N/A"])[0]
             print(f"Gene: {gene}, Product: {product}\n")
+# %%
+# json_directory = "/Users/YoshitakaM/Downloads/mibig_json_minimal"
+json_directory = "/Users/YoshitakaM/Downloads/mibig_json_3.1"
+# すべてのjsonファイルをmibig_dataのdictに変換
+mibig_data = {}
+for json_file in Path(json_directory).glob("*.json"):
+    with open(json_file, "r") as f:
+        mibig_data[json_file.stem] = json.load(f)
+
+# %%
+# mibig_data[*]["cluster"]["biosyn_class"]の値をすべて表示
+all_biosyn_classes = set()
+for _, value in mibig_data.items():
+    biosyn_classes = value["cluster"]["biosyn_class"]
+    all_biosyn_classes.update(biosyn_classes)
+
+for biosyn_class in all_biosyn_classes:
+    print(biosyn_class)
 # %%
